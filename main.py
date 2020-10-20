29412f453f01a100a579939784b52b854d174d44
retasu0141 <47290741+retasu0141@users.noreply.github.com>
2020-10-20 13:30:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -619,21 +619,24 @@ def handle_message(event):
                     print (str(e))
                     line_bot_api.reply_message(msg_from,TextSendMessage(text='失敗！'))
         if setting2[user_id]['setting2'] == True and user_id == setting_[user_id]['ID']:
+            setting2[user_id]['setting2'] = False
+            setting2[user_id]['setting3'] = True
+        if setting2[user_id]['setting3'] == True and user_id == setting_[user_id]['ID']:
             try:
                 print('ok-12')
                 setting_youtube[user_id]['text'] = msg_text
-                setting2[user_id]['setting2'] = False
-                setting2[user_id]['setting3'] = True
+                setting2[user_id]['setting3'] = False
+                setting2[user_id]['setting4'] = True
                 items = {'items': [{'type': 'action','action': {'type': 'message','label': '音楽','text': '音楽'}},{'type': 'action','action': {'type': 'message','label': 'おもしろ','text': 'おもしろ'}},{'type': 'action','action': {'type': 'message','label': 'VTuber','text': 'VTuber'}}]}
                 line_bot_api.reply_message(msg_from,TextSendMessage(text='コメントを設定できたよ！\n次は動画のカテゴリーを決めて送信してね！\nカテゴリーは好きに作れるよ！\n\n迷ったときは下のボタンからも送信できるよ！',quick_reply=items))
             except Exception as e:
                 print (str(e))
                 line_bot_api.reply_message(msg_from,TextSendMessage(text='失敗！'))
 
-        if setting2[user_id]['setting3'] == True and user_id == setting_[user_id]['ID']:
+        if setting2[user_id]['setting4'] == True and user_id == setting_[user_id]['ID']:
             try:
                 print('ok-13')
-                setting2[user_id]['setting3'] = False
+                setting2[user_id]['setting4'] = False
                 setting_youtube[user_id]['ctg'] = msg_text
                 youtube_set(setting_youtube[user_id]['url'],setting_youtube[user_id]['text'],0,[user_id],setting_youtube[user_id]['ctg'])
                 line_bot_api.reply_message(msg_from,TextSendMessage(text='アップできたよ！'))
