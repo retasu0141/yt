5325301e8f3a594b7e879e7c95a29744758ef17e
retasu0141 <47290741+retasu0141@users.noreply.github.com>
2020-10-20 15:43:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -179,7 +179,13 @@ def Flex(date):
     l = requests.Session().get('https://www.googleapis.com/youtube/v3/videos', params=payload)
     resp_dict = json.loads(l.content)
     title = resp_dict['items'][0]['snippet']['title']
-    sum_ = resp_dict['items'][0]['snippet']['thumbnails']['standard']['url']
+    try:
+        sum_ = resp_dict['items'][0]['snippet']['thumbnails']['standard']['url']
+    except:
+        try:
+            sum_ = resp_dict['items'][0]['snippet']['thumbnails']['medium']['url']
+        except:
+            sum_ = resp_dict['items'][0]['snippet']['thumbnails']['default']['url']
     data = {
   "type": "bubble",
   "hero": {
